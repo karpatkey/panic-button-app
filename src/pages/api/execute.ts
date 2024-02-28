fe54a51eb283efad18e565a22d8b07613f226444
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-02-28 11:49:13 +0000

    feat: wip execute signed transactions with web3signer
    - use local forks in dev
    - web3signer instance per chain
    - set static chainId on ethers provider
    - don't wait on transaction

--- src/pages/api/execute.ts
+++ src/pages/api/execute.ts
@@ -17,8 +17,6 @@ type Status = {
   error?: Maybe<string>
 }
 
-const WEB3SIGNER_URL = process.env.WEB3SIGNER_URL
-
 // Create a mapper for DAOs
 const DAO_MAPPER: Record<string, string> = {
   'Gnosis DAO': 'GnosisDAO',
@@ -183,7 +181,65 @@ export default withApiAuthRequired(async function handler(
     }
   }
 
-  if (execution_type === 'simulate' || (execution_type === 'execute' && !WEB3SIGNER_URL)) {
+  // TODO remove this labelled break as no one uses this in JS
+  // it's so that we cancel execution with signer if it's not configured
+  // and continues to the "test" execution
+  break_execute: if (execution_type === 'execute') {
+    try {
+      const { transaction } = req.body as {
+        transaction: Maybe<any>
+      }
+
+      const signerUrl = getSignerUrl(transaction.chainId)
+      if (!signerUrl) {
+        // TODO change this
+        break break_execute
+      }
+
+      // const priv = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+      // const wallet = new ethers.Wallet(priv)
+      // const address = wallet.address
+
+      // delete transaction.chainId
+      // transaction.from = address
+      // console.log(transaction)
+
+      const id = +new Date()
+      const headers = { 'Content-Type': 'application/json' }
+      const payload = {
+        jsonrpc: '2.0',
+        method: 'eth_signTransaction',
+        params: [transaction],
+        id: id,
+      }
+      // console.log('payload', payload)
+      // console.log(signerUrl)
+      const response = await fetch(signerUrl, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(payload),
+      })
+      const signed = await response.json()
+      // const t = await wallet.populateTransaction(transaction)
+      // const ethersSigned = await wallet.signTransaction(t)
+      // console.log('ethersSigned', ethersSigned)
+
+      if (signed.error) {
+        throw new Error('Web3Signer Error: ' + JSON.stringify(signed.error))
+      }
+      // console.log('signed', signed)
+      const provider = getEthersProvider(blockchain)
+      const txResponse = await provider.broadcastTransaction(signed.result)
+      // await txResponse.wait()
+
+      return res.status(200).json({ data: { tx_hash: txResponse.hash } })
+    } catch (error) {
+      console.error('EXECUTION_ERROR: ', error)
+      return res.status(500).json({ error: (error as Error)?.message, status: 500 })
+    }
+  }
+
+  if (execution_type === 'simulate' || execution_type === 'execute') {
     try {
       // Build de arguments for the transaction builder
 
@@ -208,58 +264,64 @@ export default withApiAuthRequired(async function handler(
     }
   }
 
-  if (execution_type === 'execute' && WEB3SIGNER_URL) {
-    try {
-      const { transaction } = req.body as {
-        transaction: Maybe<any>
-      }
-
-      const id = +new Date()
-      const headers = { 'Content-Type': 'application/json' }
-      const payload = {
-        jsonrpc: '2.0',
-        method: 'eth_signTransaction',
-        params: [transaction],
-        id: id,
-      }
-      const response = await fetch(WEB3SIGNER_URL, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(payload),
-      })
-      const signed = await response.json()
-      if (signed.error) {
-        throw new Error('Web3Signer Error: ' + JSON.stringify(signed.error))
-      }
-      const provider = getEthersProvider(blockchain)
-      const txResponse = await provider.broadcastTransaction(signed.result)
-      await txResponse.wait()
-
-      return res.status(200).json({ data: { tx_hash: txResponse.hash } })
-    } catch (error) {
-      console.error('EXECUTION_ERROR: ', error)
-      return res.status(500).json({ error: (error as Error)?.message, status: 500 })
-    }
-  }
-
   return res.status(500).json({ error: 'Internal Server Error', status: 500 })
 })
 
 function getEthersProvider(blockchain: BLOCKCHAIN) {
   const { main, fallback } = {
     Ethereum: {
-      main: process?.env?.ETHEREUM_RPC_ENDPOINT,
-      fallback: process?.env?.ETHEREUM_RPC_ENDPOINT_FALLBACK,
+      production: {
+        main: process?.env?.ETHEREUM_RPC_ENDPOINT,
+        fallback: process?.env?.ETHEREUM_RPC_ENDPOINT_FALLBACK
+      },
+      development: {
+        main: `http://${process?.env?.LOCAL_FORK_HOST_ETHEREUM}:${process?.env?.LOCAL_FORK_PORT_ETHEREUM}`,
+        fallback: ''
+      },
+      test: {
+        main: '',
+        fallback: ''
+      }
     },
     Gnosis: {
-      main: process?.env?.GNOSIS_RPC_ENDPOINT,
-      fallback: process?.env?.GNOSIS_RPC_ENDPOINT_FALLBACK,
-    },
-  }[blockchain]
+      production: {
+        main: process?.env?.GNOSIS_RPC_ENDPOINT,
+        fallback: process?.env?.GNOSIS_RPC_ENDPOINT_FALLBACK
+      },
+      development: {
+        main: `http://${process?.env?.LOCAL_FORK_HOST_GNOSIS}:${process?.env?.LOCAL_FORK_PORT_GNOSIS}`,
+        fallback: ''
+      },
+      test: {
+        main: '',
+        test: ''
+      }
+    }
+  }[blockchain][process.env.NODE_ENV]
 
+  const network = { Ethereum: 1, Gnosis: 100 }[blockchain]
+  const options = [ethers.Network.from(network), { staticNetwork: true }] as [
+    ethers.Networkish,
+    ethers.JsonRpcApiProviderOptions
+  ]
   const provider = new ethers.FallbackProvider([
-    { provider: new ethers.JsonRpcProvider(main), priority: 1 },
-    { provider: new ethers.JsonRpcProvider(fallback), priority: 100 },
+    {
+      provider: new ethers.JsonRpcProvider(main, ...options),
+      priority: 1
+    },
+    {
+      provider: new ethers.JsonRpcProvider(fallback, ...options),
+      priority: 100
+    }
   ])
   return provider
+}
+
+function getSignerUrl(chainId: string) {
+  return (
+    {
+      '1': process.env.WEB3SIGNER_ETHEREUM_URL,
+      '100': process.env.WEB3SIGNER_GNOSIS_URL
+    }[chainId] ?? ''
+  )
 }
