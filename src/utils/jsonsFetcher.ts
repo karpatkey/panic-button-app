b9359e441a9d294a962265f789f6e318420bb4e7
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-01 22:34:39 +0000

    feat: improve json fetch
    - cache by 10min
    - fix dao mapper

--- src/utils/jsonsFetcher.ts
+++ src/utils/jsonsFetcher.ts
@@ -5,15 +5,6 @@ const MINIO_SECRET_KEY = process.env.MINIO_SECRET_KEY ?? ''
 const MINIO_ACCESS_KEY = process.env.MINIO_ACCESS_KEY ?? ''
 const MINIO_BUCKET = process.env.MINIO_BUCKET ?? ''
 
-function streamBucketToString<T>(stream: Minio.BucketStream<T>): Promise<T[]> {
-  const chunks = [] as T[]
-  return new Promise<T[]>((resolve, reject) => {
-    stream.on('data', (chunk) => chunks.push(chunk))
-    stream.on('error', (err) => reject(err))
-    stream.on('end', () => resolve(chunks))
-  })
-}
-
 interface File {
   dao: string
   blockchain: string
@@ -23,19 +14,90 @@ interface File {
 
 const DAO_NAME_MAPPER = {
   GnosisDAO: 'Gnosis DAO',
-  GnosisLTD: 'Gnosis LTD',
+  GnosisLtd: 'Gnosis LTD',
   karpatkey: 'karpatkey DAO',
   BalancerDAO: 'Balancer DAO',
-  ENSDAO: 'ENS DAO',
-  CoWDAO: 'CoW DAO',
+  ENS: 'ENS DAO',
+  CoW: 'CoW DAO',
   GnosisGuild: 'Gnosis Guild'
 } as any
 
+function streamBucketToString<T>(stream: Minio.BucketStream<T>): Promise<T[]> {
+  const chunks = [] as T[]
+  return new Promise<T[]>((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(chunk))
+    stream.on('error', (err) => reject(err))
+    stream.on('end', () => resolve(chunks))
+  })
+}
+
+async function fetchJsons(): Promise<File[]> {
+  // // Simulate errors :)
+  // if (Math.random() > 0.7) {
+  //   throw new Error('BOOM!')
+  // }
+  const minioClient = new Minio.Client({
+    endPoint: MINIO_ENDPOINT,
+    accessKey: MINIO_ACCESS_KEY,
+    secretKey: MINIO_SECRET_KEY,
+    useSSL: true
+  })
+
+  const objectsStream = minioClient.listObjects(MINIO_BUCKET)
+
+  const objects = await streamBucketToString(objectsStream)
+  const res = objects
+    .filter((o) => o.name?.endsWith('_strategies.json'))
+    .map(async (object) => {
+      const stream = await minioClient.getObject(MINIO_BUCKET, object.name ?? '')
+
+      const streamData = new Promise((resolve, reject) => {
+        let data = ''
+        stream.on('data', (chunk) => (data += chunk))
+        stream.on('end', () => resolve(data))
+        stream.on('error', (err) => reject(err))
+      })
+
+      return streamData.then((content: any) => JSON.parse(content))
+    })
+
+  return await Promise.all(res)
+}
+
+const REFRESH_AFTER = 10 * 60 * 1000 // 10 * 60 * 1000 == 10 minutes
+let LAST_REFRESH = +new Date() - REFRESH_AFTER
+type Cache = File[] | null
+let CACHE: Cache
+
+function invalidCache() {
+  return LAST_REFRESH < +new Date() - REFRESH_AFTER
+}
+
+async function refreshCache(fn: () => Promise<Cache>) {
+  if (invalidCache()) {
+    console.time('[Strategies] Refetching json configs')
+    try {
+      CACHE = await fn()
+    } catch (e) {
+      console.error('Error fetching strategies files.' + (CACHE ? ' Using outdated version' : ''))
+      console.error(e)
+    }
+    console.timeEnd('[Strategies] Refetching json configs')
+    LAST_REFRESH = +new Date()
+  }
+}
+
+export async function cached(fn: () => Promise<Cache>) {
+  if (CACHE) {
+    refreshCache(fn)
+  } else {
+    await refreshCache(fn)
+  }
+  return CACHE
+}
+
 export async function getDaosConfigs(daos: string[]) {
-  const label = 'Fetch json configs'
-  console.time(label)
-  const configs = await fetchJsons()
-  console.timeEnd(label)
+  const configs = await cached(fetchJsons)
 
   if (!configs) {
     return []
@@ -50,37 +112,4 @@ export async function getDaosConfigs(daos: string[]) {
     .filter((f) => {
       return daos.includes(f.dao)
     })
-}
-
-async function fetchJsons(): Promise<File[]> {
-  const minioClient = new Minio.Client({
-    endPoint: MINIO_ENDPOINT,
-    accessKey: MINIO_ACCESS_KEY,
-    secretKey: MINIO_SECRET_KEY,
-    useSSL: true
-  })
-
-  const objectsStream = minioClient.listObjects(MINIO_BUCKET)
-
-  const objects = await streamBucketToString(objectsStream)
-  const res = objects.map(async (object) => {
-    const stream = await minioClient.getObject(MINIO_BUCKET, object.name ?? '')
-
-    const pro = new Promise((resolve) => {
-      let data = ''
-      stream.on('data', function (chunk) {
-        data += chunk
-      })
-      stream.on('end', function () {
-        resolve(data)
-      })
-      stream.on('error', function (err) {
-        console.log(err)
-      })
-    })
-
-    return pro.then((str: any) => JSON.parse(str))
-  })
-
-  return await Promise.all(res)
 }
