25c62eb38ff108f5bb77b3c5419bc98dc10a58e9
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-08 15:23:13 +0000

    test: ci

--- src/pages/positions/index.tsx
+++ src/pages/positions/index.tsx
@@ -51,6 +51,8 @@ export const getServerSideProps = async (context: {
   const { req, res } = context
   const session = await getSession(req as any, res as any)
 
+  console.log('Position page')
+
   if (!session) {
     return {
       props: {
