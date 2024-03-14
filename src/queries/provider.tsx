67930a2f7e24d9c4ec9bbab43e472f433bdbc860
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-14 20:09:52 +0000

    feat: persistent caching of positions

---
+++ src/queries/provider.tsx
@@ -0,0 +1,29 @@
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { useMemo } from 'react'
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  const [queryClient, persister] = useMemo(() => {
+    const qc = new QueryClient({
+      defaultOptions: {
+        queries: {
+          // With SSR, we usually want to set some default staleTime
+          // above 0 to avoid refetching immediately on the client
+          staleTime: 1 * 60 * 1000,
+          gcTime: 24 * 60 * 60 * 1000, // 1 day, cached
+        },
+      },
+    })
+    const persister = createSyncStoragePersister({
+      storage: window.localStorage,
+    })
+    return [qc, persister]
+  }, [])
+
+  return (
+    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+      {children}
+    </PersistQueryClientProvider>
+  )
+}
