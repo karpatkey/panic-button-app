ee1682ddf163ce5bbb4dbb7a22074762024eb61c
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-18 10:00:17 +0000

    feat: show dao and balance usd amount in config screen

--- src/views/Position/Title/Secondary.tsx
+++ src/views/Position/Title/Secondary.tsx
@@ -1,13 +1,13 @@
-import BoxWrapperRow from 'src/components/Wrappers/BoxWrapperRow'
 import CustomTypography from 'src/components/CustomTypography'
-import * as React from 'react'
+import BoxWrapperRow from 'src/components/Wrappers/BoxWrapperRow'
 
 interface TitleSecondaryProps {
   title: string
   subtitle?: string
+  children?: React.ReactNode
 }
 
-const Secondary = ({ title, subtitle }: TitleSecondaryProps) => {
+const Secondary = ({ title, subtitle, children }: TitleSecondaryProps) => {
   return (
     <BoxWrapperRow gap={2} sx={{ justifyContent: 'flex-start' }}>
       <CustomTypography
@@ -17,12 +17,12 @@ const Secondary = ({ title, subtitle }: TitleSecondaryProps) => {
           fontStyle: 'normal',
           fontSize: 18,
           lineHeight: '18px',
-          color: 'custom.grey.dark'
+          color: 'custom.grey.dark',
         }}
       >
         {title}
       </CustomTypography>
-      {subtitle ? (
+      {subtitle || children ? (
         <CustomTypography
           sx={{
             fontWeight: 700,
@@ -30,10 +30,10 @@ const Secondary = ({ title, subtitle }: TitleSecondaryProps) => {
             fontStyle: 'normal',
             fontSize: 18,
             lineHeight: '18px',
-            color: 'custom.grey.dark'
+            color: 'custom.grey.dark',
           }}
         >
-          {subtitle}
+          {subtitle || children}
         </CustomTypography>
       ) : null}
     </BoxWrapperRow>
