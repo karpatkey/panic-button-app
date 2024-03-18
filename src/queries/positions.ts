7c28d6dbfdd0a75ed62a6b340f4eb7cc7047f656
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-18 18:25:02 +0000

    fix: fetching positions after login

--- src/queries/positions.ts
+++ src/queries/positions.ts
@@ -1,39 +1,54 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
 import { useQuery } from '@tanstack/react-query'
 import { useMemo } from 'react'
 import { Position } from 'src/contexts/state'
 import { slug } from 'src/utils/string'
 
 export function usePositions() {
+  const { user } = useUser()
+
   return useQuery<Position[]>({
-    queryKey: ['positions'],
-    queryFn: () => fetch('/api/positions').then((res) => res.json()),
+    queryKey: ['positions', user?.sub],
+    queryFn: async () => {
+      const res = await fetch('/api/positions')
+      const body = await res.json()
+      if (!body || body.error) {
+        throw new Error(body.error)
+      }
+      return body
+    },
   })
 }
 
 export function usePosition(dao: string, chain: string, pool_id: string) {
   const q = usePositions()
-  const { data } = q
+  const { data, error } = q
 
   const position = useMemo(() => {
-    if (q.data) {
+    if (data && !error) {
       return findPosition(data ?? [], { dao, chain, pool_id })
+    } else {
+      return null
     }
-  }, [q.data, data, dao, chain, pool_id])
+  }, [data, error, dao, chain, pool_id])
 
   return useMemo(() => {
     return {
       ...q,
+      error: error,
       data: position,
     }
-  }, [q, position])
+  }, [q, error, position])
 }
 
 function findPosition(
   positions: Position[],
   { dao, chain, pool_id }: { dao: string; chain: string; pool_id: string },
-) {
-  return positions.find(
-    (position) =>
-      slug(position.dao) == dao && position.blockchain == chain && position.pool_id == pool_id,
+): Position | null {
+  return (
+    positions.find(
+      (position) =>
+        slug(position.dao) == dao && position.blockchain == chain && position.pool_id == pool_id,
+    ) || null
   )
 }
