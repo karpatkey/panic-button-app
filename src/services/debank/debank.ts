597983763751a4717d52a8c137b34ab1288a9f3c
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-21 16:43:22 +0000

    feat: improve debank
    - better types
    - fetch portocol tokens
    - cache debank response on server side
    - Don't parse unused tokens in protocol like reward

--- src/services/debank/debank.ts
+++ src/services/debank/debank.ts
@@ -13,13 +13,31 @@ function retryBackoff(retriesLeft: number) {
   return extraSecondPerRetry + randomPart
 }
 
-async function getDebankWalletData(wallet: string) {
-  return await getDebank(`/v1/user/all_complex_protocol_list?id=${wallet}`)
+async function fetchDebankWalletData(wallet: string) {
+  return await getDebank(`/v1/user/all_complex_protocol_list?chain_ids=eth,xdai&id=${wallet}`)
 }
 
-async function getDebankTokensData(wallet: string) {
-  return await getDebank(`/v1/user/all_token_list?isAll=false&id=${wallet}`)
+async function fetchDebankTokensData(wallet: string) {
+  return await getDebank(`/v1/user/all_token_list?isAll=true&id=${wallet}`)
 }
+
+async function fetchWallet(wallet: string) {
+  console.log(`CALLED FETCH_WALLET ${wallet}`)
+  const p1 = fetchDebankWalletData(wallet)
+  const p2 = fetchDebankTokensData(wallet)
+  const positions = await p1
+  const tokens = await p2
+  return { positions, tokens }
+}
+
+// Switch to this one for run as script in json-builder
+// const getFromDebank = fetchWallet
+
+// Use caching
+import ExpiryMap from 'expiry-map'
+import pMemoize from 'p-memoize'
+const cache = new ExpiryMap(20 * 60 * 1000)
+const getFromDebank = pMemoize(fetchWallet, { cache })
 
 async function getDebank(path: string, retriesLeft: number = MAX_RETRIES) {
   try {
@@ -40,17 +58,98 @@ async function getDebank(path: string, retriesLeft: number = MAX_RETRIES) {
   }
 }
 
-async function transformData(wallet: string, positions: any, tokens: any) {
+export interface DebankToken {
+  name: string
+  symbol: string
+  display_symbol: string
+  optimized_symbol: string
+  decimals: number
+  logo_url: string
+  protocol_id: string
+  price: number
+  is_core: boolean
+  is_wallet: boolean
+  time_at: number
+  amount: number
+  raw_amount: number
+  id: string
+  chain: string
+}
+export interface DebankPositionItem {
+  stats: {
+    asset_usd_value: number
+    debt_usd_value: number
+    net_usd_value: number
+  }
+  update_at: number
+  name: string
+  detail_types: string[]
+  pool: {
+    id: string
+  }
+  detail: {
+    description: string
+    supply_token_list: {
+      id: string
+      chain: string
+      name: string
+      symbol: string
+      display_symbol?: string
+      optimized_symbol: string
+      decimals: number
+      logo_url: string
+      protocol_id: string
+      price: number
+      is_verified: boolean
+      is_core: boolean
+      is_wallet: boolean
+      time_at: number
+      amount: number
+    }[]
+  }
+}
+export interface DebankPosition {
+  id: string
+  chain: string
+  name: string
+  site_url: string
+  logo_url: string
+  has_supported_portfolio: boolean
+  tvl: number
+  portfolio_item_list: DebankPositionItem[]
+}
+
+export type ResponsePosition = {
+  usd_amount: number
+  chain: string
+  position_type: string
+  pool_id: string
+  protocol_name: string
+  lptoken_name: string
+  updated_at: number
+  tokens: any[]
+}
+type ResponseToken = DebankToken
+
+interface DebankFullResponse {
+  wallet: string
+  positions: ResponsePosition[]
+  tokens: ResponseToken[]
+}
+
+async function transformData(
+  wallet: string,
+  positions: DebankPosition[],
+  tokens: DebankToken[],
+): Promise<DebankFullResponse> {
   return {
-    positions: positions.flatMap((i: any) => transformEntry(wallet, i)),
-    tokens: tokens,
+    positions: positions.flatMap(transformPosition),
+    tokens: tokens.flatMap(transformToken),
     wallet: wallet,
   }
 }
 
 const Chains = new Map([
-  ['eth', 'ethereum'],
-  ['xdai', 'gnosis'],
   ['eth', 'ethereum'],
   ['xdai', 'gnosis'],
   ['matic', 'polygon'],
@@ -67,30 +166,30 @@ const Protocols = new Map([
   ['LIDO', 'Lido'],
 ])
 
-// function transformPoolId(pool_id) {
-//   return pool_id == '0xde3b7ec86b67b05d312ac8fd935b6f59836f2c41'
-//     ? '0xFEdb19Ec000d38d92Af4B21436870F115db22725'
-//     : pool_id
-// }
-
 import kitchen_data from './kitchen.json'
 const PositionNamesFromPoolId = new Map(
   kitchen_data.map((d) => [d.lptoken_address, d.lptoken_name]),
 )
+function transformToken(token: DebankToken) {
+  return {
+    ...token,
+    chain: Chains.get(token.chain) || token.chain,
+  }
+}
 
-function transformEntry(wallet: string, entry: any) {
-  const protocol_name = Protocols.get(entry.name) || entry.name
-  const chain = Chains.get(entry.chain)
+function transformPosition(position: DebankPosition): ResponsePosition[] {
+  const protocol_name = Protocols.get(position.name) || position.name
+  const chain = Chains.get(position.chain) || position.chain
   const tokenOfType =
     (t: any) =>
-    ({ chain, symbol, amount, price }: any) => ({ chain, symbol, amount, price, as: t })
+    ({ symbol, amount, price }: any) => ({ symbol, amount, price, as: t })
 
-  return entry.portfolio_item_list.flatMap((i: any) => {
+  return position.portfolio_item_list.flatMap((i) => {
     const tokens = [
-      ...(i.detail.reward_token_list || []).map(tokenOfType('reward')),
       ...(i.detail.supply_token_list || []).map(tokenOfType('supply')),
-      ...(i.detail.borrow_token_list || []).map(tokenOfType('borrow')),
-      ...(i.detail.other_tokens_list || []).map(tokenOfType('other')),
+      // ...(i.detail.reward_token_list || []).map(tokenOfType('reward')),
+      // ...(i.detail.borrow_token_list || []).map(tokenOfType('borrow')),
+      // ...(i.detail.other_tokens_list || []).map(tokenOfType('other')),
     ].filter((l) => l)
 
     const position_type = i.name
@@ -98,9 +197,9 @@ function transformEntry(wallet: string, entry: any) {
     const lptoken_name = PositionNamesFromPoolId.get(pool_id) || i.detail.description
 
     return {
+      debank: i,
       usd_amount: i.stats.asset_usd_value,
       chain,
-      wallet,
       position_type,
       pool_id,
       protocol_name,
@@ -113,14 +212,11 @@ function transformEntry(wallet: string, entry: any) {
 
 export async function getPositions(wallets: string[]) {
   const processWallet = async (wallet: string) => {
-    const protocolsP = getDebankWalletData(wallet)
-    const tokensP = getDebankTokensData(wallet)
-    const protocols = await protocolsP
-    const tokens = await tokensP
-    if (protocols.message || tokens.message) {
-      throw new Error(protocols.message || tokens.message)
+    const { positions, tokens } = await getFromDebank(wallet)
+    if (positions.message || tokens.message) {
+      throw new Error(positions.message || tokens.message)
     }
-    return transformData(wallet, protocols, tokens)
+    return transformData(wallet, positions, tokens)
   }
   return await Promise.all(wallets.flatMap(processWallet))
 }
